77e97541a976c97f0fb5b4bee1145874fc14cbf9
Rajiv Ramdhany <nirish777@gmail.com>
2022-02-27 20:13:45 +0000

    Removes unecessary protoc compiler invocation script as grpc sources are generated via build process

--- src/proto/build.sh
+++
@@ -1,6 +0,0 @@
-echo "Generating js protobuf definitions"
-# protoc --js_out=import_style=commonjs,binary:. numseq.proto
-# protoc -I=./ --java_out=./ numseq.proto
-
-echo "Generating java protobuf definitions"
-../../../protoc-3.19.4-osx-x86_64/bin/protoc -I=./ --java_out=../server/num-sequence-service/src/main/java/ numseq.proto
